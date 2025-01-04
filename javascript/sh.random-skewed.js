86f90c0a23f1efec89134350f157bd4588bb9242
Steve Horne <stevehorne@gmail.com>
2025-01-04 10:51:29 -0600

    refactoring some JS

--- javascript/sh.random-skewed.js
+++ javascript/sh.random-skewed.js
@@ -1,4 +1,4 @@
-var skewedTo = 0.5;
+let skewedTo = 0.5;
 
 declareattribute("skewedTo", null, null, 1);
 
@@ -7,16 +7,16 @@ function bang() {
 }
 
 function skew(skewTo, stdDev) {
-    var rand = (Math.random() * 2 - 1) + (Math.random() * 2 - 1) + (Math.random() * 2 - 1);
+    let rand = (Math.random() * 2 - 1) + (Math.random() * 2 - 1) + (Math.random() * 2 - 1);
     return skewTo + rand * stdDev;
 }
 
 function getRandom(skewTo) {
-    var difference = Math.min(skewTo, 1.0 - skewTo);
-    var steps = 5;
-    var total = 0.0;
-    for (var i = 1; i <= steps; i++)
+    let difference = Math.min(skewTo, 1.0 - skewTo);
+    let steps = 5;
+    let total = 0.0;
+    for (let i = 1; i <= steps; i++)
         total += skew(skewTo, 1.0 * i * difference / steps);
-    return total / steps
+    return total / steps;
 
 }
